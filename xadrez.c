fdd3a1ea8c065858947d9f30e964b15f53654a04
CleytonDiasx <cleytondias.dev@gmail.com>
2025-08-16 14:03:32 +0000

    xadrez2

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
+void movRainha(int r){
+    if( r > 0){
+        printf("Esquerda\n");
+        movRainha(r - 1);
+    }
+}
+void movTorre(int t){
+    if(t > 0){
+        printf("Direita\n");
+        movTorre(t - 1);
+    }
+}
+void movBispo(int b){
+    if(b > 0){
+        printf("cimaDireita\n");
+        movBispo(b - 1);
+    }
+}
 int main(){
 
+    int nr = 8,  nb = 5, nt = 5;
     int i;//contador;
     int q;//quantidade;
     char movimento[20];//nome da direcao(EX. esquerda, direita, cima e baixo);
     char mov[20];//movimento 2 do cavalo para manter o movimento em L;
     int opcao;//usado para a estrutura switch;
 
-    //menu do jogo com 3 pecas iniciais;
+    //menu do jogo com 4 pecas iniciais;
     printf("Qual peca voce deseja mover...\n");
     printf("1. Torre;\n");
     printf("2. Rainha;\n");
@@ -18,47 +37,22 @@ int main(){
     scanf("%d", &opcao);
 
     // mensagem de invalido caso digite 4 no menu acima:
-    if (opcao < 1 || opcao > 4) {
+    if (opcao < 1 || opcao > 4){
         printf("Opcao invalida!\n");
         return 1;
     }
     //estrutura apos escilher uma das opcoes acima;
     switch(opcao){    
         case 1://movimento de torre;
-        printf("Digite a quantidade de vezes que voce quer mover a torre: ");
-        scanf("%d", &q);
-        printf("Digite a direcao que voce quer mover a torre:  ");
-        scanf("%s", movimento);
-
-            for(i = 0; i < q; i++){
-                printf("%s\n", movimento);
-            }
-            break;
+            movTorre(nt);
+        break;
 
         case 2://movimento da rainha;
-        printf("Digite a quantidade de vezes que voce quer mover a rainha: ");
-        scanf("%d", &q);
-        printf("Digite a direcao que voce quer mover a rainha: ");
-        scanf("%s", movimento);
-        
-            i = 0;
-            while(i < q){
-                printf("%s\n", movimento);
-                i++;
-            }
-            break;
+            movRainha(nr);
+        break;
         case 3://movimento do bispo ;
-        printf("Digite a quantidade de vezes que voce quer mover o bispo: ");
-        scanf("%d", &q);
-        printf("Digite a direcao que voce quer mover o bispo: ");
-        scanf("%s", movimento);
-
-            i = 0;
-            do{
-                printf("%s\n", movimento);
-                i++;
-            }while(i < q);
-            break;
+            movBispo(nb);
+        break;
         case 4://movimento cavalo;
         printf("Digite a direcao que voce quer mover o cavalo (cima / baixo): ");
         scanf("%s", movimento);
@@ -73,6 +67,5 @@ int main(){
             }
             break;
     }
-
     return 0;
 }
